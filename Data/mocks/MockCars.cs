a08a15e2e7580ce90bdcccc8407c78f3b329f713
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-02-24 10:32:17 +0300

    Добавьте файлы проекта.

---
+++ Data/mocks/MockCars.cs
@@ -0,0 +1,86 @@
+﻿using Saite_1.Data.interfaces;
+using Saite_1.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Saite_1.Data.mocks
+{
+    public class MockCars : IAllCars
+    {
+        private readonly ICarsCategory _categoryCars = new MockCategory();
+        public IEnumerable<Car> Cars
+        {
+            get
+            {
+                return new List<Car>
+                {
+                    new Car
+                    {
+                        name = "Tesla Model S",
+                        shortDesc = "Быстрый автомобиль",
+                        longDesc = "Красивый, быстрый и тихий автомобил кампании Tesla",
+                        img = "/img/tesla.jpg",
+                        price = 11000,
+                        isFavourite = true,
+                        available = true,
+                        Category = _categoryCars.AllCategories.First()
+                    },
+                    new Car
+                    {
+                        name = "Ford Fiesta",
+                        shortDesc = "Тихий и спокойный",
+                        longDesc = "Удобный автомобиль для городской жизни",
+                        img = "/img/fiesta.jpg",
+                        price = 4500,
+                        isFavourite = true,
+                        available = true,
+                        Category = _categoryCars.AllCategories.Last()
+                    },
+                    new Car
+                    {
+                        name = "BMV M3",
+                        shortDesc = "Дерзкий и стильный",
+                        longDesc = "Удобный автомобиль для городской жизни",
+                        img = "/img/bmw.jpg",
+                        price = 65000,
+                        isFavourite = true,
+                        available = true,
+                        Category = _categoryCars.AllCategories.Last()
+                    },
+                    new Car
+                    {
+                        name = "Mercedes C Class",
+                        shortDesc = "Уютный и большой",
+                        longDesc = "Удобный автомобиль для городской жизни",
+                        img = "/img/mercedes.jpg",
+                        price = 40000,
+                        isFavourite = true,
+                        available = true,
+                        Category = _categoryCars.AllCategories.Last()
+                    },
+                    new Car
+                    {
+                        name = "Nissan Leaf",
+                        shortDesc = "Бесшумный и экологичный",
+                        longDesc = "Удобный автомобиль для городской жизни",
+                        img = "/img/leaf.jpeg",
+                        price = 14000,
+                        isFavourite = true,
+                        available = true,
+                        Category = _categoryCars.AllCategories.First()
+                    }
+
+
+                };
+            }
+        }
+        public IEnumerable<Car> getFavCars { get; set; }
+
+        public Car getObjectCar(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
